42ca6ac9916f9084ee8b9fada6ca67ec10c75f09
Omar <Omar>
2021-12-20 00:39:47 -0500

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app,
-  get("*", (req, res) => {
-    res.status(404).send("<h1>404 Page</h1>");
-  });
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
 
 app.listen(process.env.PORT);
