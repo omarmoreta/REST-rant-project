d0a5714abad6540a4e7a12c075fbf6c358f6d189
Omar <Omar>
2022-01-03 05:41:12 -0500

    added routes table to README

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-require("dotenv").config();
+// DEPENDENCIES
 const express = require("express");
+
+// CONFIGURATION
+require("dotenv").config();
+const PORT = process.env.PORT;
 const app = express();
 
+// ROUTES
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
@@ -12,4 +17,5 @@ app.get("*", (req, res) => {
   res.status(404).send("<h1>404 Page</h1>");
 });
 
-app.listen(process.env.PORT);
+// LISTEN
+app.listen(PORT);
