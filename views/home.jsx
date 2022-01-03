abb7dca87b9e286606ceb82e38861146cf4b8292
Omar <Omar>
2022-01-03 17:03:39 -0500

    added image and css

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/waffles.jpg" alt="Waffles dripped with syrup"/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/photos/GJAHkC6UMf0">Lindsay Moe</a> on <a href="https://unsplash.com/@lindsaymoe">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
