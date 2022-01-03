e59daf785992d5b7ce4a73a3d9d1233e06b64bd4
Omar <Omar>
2022-01-03 15:17:30 -0500

    added 404 page view

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
+  res.render("error404");
+  // res.status(404).send("<h1>404 Page</h1>");
 });
 
 // LISTEN
