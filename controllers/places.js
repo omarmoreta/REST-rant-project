a5116fe1ff0809cab79210759fa3358cc0753d78
Omar <Omar>
2022-01-08 06:50:07 -0500

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -38,6 +38,18 @@ router.post("/", (req, res) => {
   res.redirect("/places");
 });
 
+// // PUT /places/:id/edit
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id] });
+  }
+});
+
 // DELETE /places/:id
 router.delete("places/:id", (req, res) => {
   let id = Number(req.params.id);
---
+++ views/places/edit.jsx
@@ -0,0 +1,45 @@
+const React = require("react")
+const Def = require("../default")
+
+function edit_form (data) {
+    return (
+        <Def>
+            {/* Was not rendering*/}
+            <link rel="stylesheet" href="../style.css"/>
+            <main>
+                <h1>Edit Place</h1>
+                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="name">Place Name</label>
+                            <input className="form-control" id="name" name="name" defaultValue={data.place.name}/>
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input className="form-control" type="url" id="pic" name="pic" defaultValue={data.place.pic}/>
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="city">City</label>
+                            <input className="form-control" id="city" name="city" defaultValue={data.place.city}/>
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="state">State</label>
+                            <input className="form-control" id="state" name="state" defaultValue={data.place.state}/>
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="cuisines">Cuisines</label>
+                            <input className="form-control" id="cuisines" name="cuisines" defaultValue={data.place.cuisines} />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <br/>
+                            <input className="btn btn-primary" type="submit" value="Edit Place"/>
+                        </div>
+                    </div>   
+                     
+                </form>
+                </main>
+        </Def>
+    )
+}
+
+module.exports = edit_form
