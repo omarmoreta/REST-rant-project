7db62041b69fa18be6d3da4b3d5e23691e5b011d
Omar <Omar>
2022-01-10 01:33:50 -0500

    added mongoose and connected to mongodb

--- controllers/places.js
+++ controllers/places.js
@@ -1,89 +1,152 @@
+// const router = require("express").Router();
+// const places = require("../models/places");
+
+// // GET /places/new
+// router.get("/new", (req, res) => {
+//   res.render("places/new");
+// });
+
+// // GET /places/:id/edit
+// router.get("/:id/edit", (req, res) => {
+//   let id = Number(req.params.id);
+//   if (isNaN(id)) {
+//     res.render("error404");
+//   } else if (!places[id]) {
+//     res.render("error404");
+//   } else {
+//     res.render("places/edit", { place: places[id], id: id });
+//   }
+// });
+
+// // GET /places/:id
+// router.get("/:id", (req, res) => {
+//   let id = Number(req.params.id);
+//   if (isNaN(id)) {
+//     res.render("error404");
+//   } else if (!places[id]) {
+//     res.render("error404");
+//   } else {
+//     res.render("places/show", { place: places[id], id: id });
+//   }
+// });
+
+// // GET /places
+// router.get("/", (req, res) => {
+//   res.render("places/index", { places });
+// });
+
+// // POST /places
+// router.post("/", (req, res) => {
+//   if (!req.body.pic) {
+//     req.body.pic = "/images/goofy-404.jpg";
+//   }
+//   if (!req.body.city) {
+//     req.body.city = "Anytown";
+//   }
+//   if (!req.body.state) {
+//     req.body.state = "USA";
+//   }
+//   places.push(req.body);
+//   res.redirect("/places");
+// });
+
+// // PUT /places/:id
+// router.put("/:id", (req, res) => {
+//   let id = Number(req.params.id);
+//   if (isNaN(id)) {
+//     res.render("error404");
+//   } else if (!places[id]) {
+//     res.render("error404");
+//   } else {
+//     if (!req.body.pic) {
+//       req.body.pic = "https://unsplash.com/photos/SWnviQeRsjI";
+//     }
+//     if (!req.body.city) {
+//       req.body.city = "Anytown";
+//     }
+//     if (!req.body.state) {
+//       req.body.state = "USA";
+//     }
+//     places[id] = req.body;
+//     res.redirect(`/places/${id}`);
+//   }
+// });
+
+// // DELETE /places/:id
+// router.delete("/:id", (req, res) => {
+//   let id = Number(req.params.id);
+//   if (isNaN(id)) {
+//     res.render("error404");
+//   } else if (!places[id]) {
+//     res.render("error404");
+//   } else {
+//     places.splice(id, 1);
+//     res.redirect("/places");
+//   }
+// });
+
+// module.exports = router;
+
 const router = require("express").Router();
-const places = require("../models/places");
-
-// GET /places/new
-router.get("/new", (req, res) => {
-  res.render("places/new");
-});
-
-// GET /places/:id/edit
-router.get("/:id/edit", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    res.render("places/edit", { place: places[id], id: id });
-  }
-});
-
-// GET /places/:id
-router.get("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    res.render("places/show", { place: places[id], id: id });
-  }
-});
+const db = require("../models");
 
 // GET /places
 router.get("/", (req, res) => {
-  res.render("places/index", { places });
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 // POST /places
 router.post("/", (req, res) => {
-  if (!req.body.pic) {
-    req.body.pic = "/images/goofy-404.jpg";
-  }
-  if (!req.body.city) {
-    req.body.city = "Anytown";
-  }
-  if (!req.body.state) {
-    req.body.state = "USA";
-  }
-  places.push(req.body);
-  res.redirect("/places");
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+// GET places/new
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+// GET /places/:id
+router.get("/:id", (req, res) => {
+  res.send("GET /places/:id stub");
 });
 
 // PUT /places/:id
 router.put("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    if (!req.body.pic) {
-      req.body.pic = "https://unsplash.com/photos/SWnviQeRsjI";
-    }
-    if (!req.body.city) {
-      req.body.city = "Anytown";
-    }
-    if (!req.body.state) {
-      req.body.state = "USA";
-    }
-    places[id] = req.body;
-    res.redirect(`/places/${id}`);
-  }
+  res.send("PUT /places/:id stub");
 });
 
 // DELETE /places/:id
 router.delete("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    places.splice(id, 1);
-    console.log("DELETED");
-    res.redirect("/places");
-  }
+  res.send("DELETE /places/:id stub");
+});
+
+// GET /place/:id/edit form
+router.get("/:id/edit", (req, res) => {
+  res.send("GET edit form stub");
+});
+
+// POST /places/:id/rant
+router.post("/:id/rant", (req, res) => {
+  res.send("GET /places/:id/rant stub");
+});
+
+// DELETE /places/:id/rant/:rantId
+router.delete("/:id/rant/:rantId", (req, res) => {
+  res.send("GET /places/:id/rant/:rantId stub");
 });
 
 module.exports = router;
--- models/places.js
+++ models/places.js
@@ -1,16 +1,29 @@
-module.exports = [
-  {
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "/images/h-thai-ml.jpg",
-  },
-  {
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "/images/cat-cafe.jpg",
-  },
-];
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: "Anytown" },
+  state: { type: String, default: "USA" },
+  founded: Number,
+});
+
+module.exports = mongoose.model("Place", placeSchema);
+
+// module.exports = [
+//   {
+//     name: "H-Thai-ML",
+//     city: "Seattle",
+//     state: "WA",
+//     cuisines: "Thai, Pan-Asian",
+//     pic: "/images/h-thai-ml.jpg",
+//   },
+//   {
+//     name: "Coding Cat Cafe",
+//     city: "Phoenix",
+//     state: "AZ",
+//     cuisines: "Coffee, Bakery",
+//     pic: "/images/cat-cafe.jpg",
+//   },
+// ];
