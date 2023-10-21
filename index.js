762293e3befeaeda55778d4298739ab03873acb2
Omar <o.moreta@gmail.com>
2023-10-21 15:36:05 -0400

    deleted Procfile and added vercel.json file for deployment

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 const methodOverride = require("method-override");
+const viewEngine = require("express-react-views").createEngine()
 
 // MIDDLEWARE
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
+app.engine("jsx", viewEngine);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
