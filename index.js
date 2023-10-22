022a464ede39710fff3b3ce191d9ff100bb0b520
Omar <o.moreta@gmail.com>
2023-10-21 22:31:08 -0400

    added cors

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // DEPENDENCIES
 const express = require("express");
+const cors = require("cors");
 
 // CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 const methodOverride = require("method-override");
-const viewEngine = require("express-react-views").createEngine()
+const viewEngine = require("express-react-views").createEngine();
 
 // MIDDLEWARE
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", viewEngine);
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
