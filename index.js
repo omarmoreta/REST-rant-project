12c8e8a5b578f1fdec138e78bbfeb72f8fc77dd2
Omar <o.moreta@gmail.com>
2023-10-25 12:23:13 -0400

    removed cors and engine to test deployment

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // DEPENDENCIES
 const express = require("express");
-const cors = require("cors");
 
 // CONFIGURATION
 require("dotenv").config();
@@ -13,7 +12,6 @@ const viewEngine = require("express-react-views").createEngine();
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", viewEngine);
-app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
